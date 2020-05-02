6716d5ca490e462a58963f331f4fe907b0ad7da1
astatsuya <sharekobekko@gmail.com>
2020-05-02 21:57:58 +0900

    minifyの設定

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,17 +17,22 @@ module.exports = (env) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html"),
+        // headにjsファイルを入れたい場合はheadを指定
+        inject: "body",
       }),
     ],
     devServer: {
       contentBase: path.join(__dirname, "dist"),
-      // どのブラウザを自動で開くか指定出来る。falseにすると自動起動をオフに出来る。
+      // どのブラウザを自動で立ち上げるか。trueで標準のブラウザ。デフォルトでは立ち上がらない。
       // open: "Google Chrome",
-      open: false,
       host: "localhost",
       compress: true,
       // port番号はデフォルトで8080, 既に使用されている場合は自動で8181になる。指定したい場合はここでする。
       // port: 3000,
+      watchOptions: {
+        // 差分を検知しないディレクトリ
+        ignored: /node_modules/,
+      },
     },
   });
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require("path");
 const webpackMerge = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const outputFile = "[name].[chunkhash]";
@@ -17,8 +19,32 @@ module.exports = (env) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html"),
+        // headにjsファイルを入れたい場合はheadを指定
+        inject: "body",
+        // optimizationを指定するとproductionモードのデフォルト設定が解除されるので、ここで指定
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true,
+        },
       }),
       new CleanWebpackPlugin(),
     ],
+    optimization: {
+      minimizer: [
+        // javascriptの最適化
+        new TerserWebpackPlugin({
+          terserOptions: {
+            // consoleを削除する
+            compress: { drop_console: true },
+          },
+        }),
+        // cssの最適化
+        new OptimizeCssPlugin(),
+      ],
+    },
   });
 };
